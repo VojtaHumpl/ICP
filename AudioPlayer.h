cd61d9dffac19e75efa55640868dd5650170159b
David_Jansa1 <david.jansa1@tul.cz>
2025-02-09 23:25:59 +0100

    Add audio volum slider

--- AudioPlayer.h
+++ AudioPlayer.h
@@ -18,12 +18,14 @@ public:
     bool playSound3D(const std::string& name, float soundX, float soundY, float soundZ, float listX, float listY, float listZ, float listXDir, float listYDir, float listZDir);
     void cleanFinishedSounds();
     bool playSound3DOnce(const std::string& name, float soundX, float soundY, float soundZ, float listX, float listY, float listZ, float listXDir, float listYDir, float listZDir);
+    void setVolume(float volume);
 
 private:
     ma_engine engine;
     std::map<std::string, ma_sound*> soundBank;
     std::vector<ActiveSound> activeSounds;
     bool initialized;
+    float volume = 1.0;
 
     bool loadSound(const std::string& name, ma_sound* snd);
     void loadSystemSounds();
